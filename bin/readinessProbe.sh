b454e1a1aae48f30b8f7771a41584e70986558c8
Yongbok Kim (ruo91) <ruo91@yongbok.net>
2018-01-04 04:01:55 -0500

    ok

--- bin/readinessProbe.sh
+++ bin/readinessProbe.sh
@@ -1,1 +1,1 @@
-curl --noproxy '*' -s -u admin:admin 'http://localhost:8080/manager/jmxproxy/?get=Catalina%3Atype%3DServer\u0026att=stateName' |grep -iq 'stateName *= *STARTED'
+curl --noproxy '*' -s -u admin:admin 'http://localhost:8080/manager/jmxproxy/?get\=Catalina%3Atype%3DServer\u0026att=stateName' |grep -iq 'stateName: STARTED'
