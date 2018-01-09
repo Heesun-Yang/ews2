0bef286baf7075eb52d8f093c308b909212fe359
Heesun-Yang <hsyang@rockplace.co.kr>
2018-01-09 17:05:38 +0900

    Create setenv.sh

---
+++ bin/setenv.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+ 
+# java.rmi.server.hostname 는 Docker가 실행되는 Host의 IP 를 지정해야 한다.
+  
+JMX_OPTS=" -Dcom.sun.management.jmxremote \
+ -Dcom.sun.management.jmxremote.authenticate=false \
+ -Djava.rmi.server.hostname=jmx-hhi.ocp.seoul.test.rockplace.co.kr \
+ -Dcom.sun.management.jmxremote.port=10030 \
+ -Dcom.sun.management.jmxremote.rmi.port=10030 \
+ -Dcom.sun.management.jmxremote.ssl=false \
+ -Djava.security.egd=file:/dev/./urandom "
+  
+CATALINA_OPTS=" ${JMX_OPTS} ${CATALINA_OPTS}"
